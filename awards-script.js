73c9fa739e6bbbd12c630f084d64928c7a690e0c
Boomer <163797142+Boomerbeatdown@users.noreply.github.com>
2024-06-08 02:16:20 -0500

    edited awards page

--- awards-script.js
+++ awards-script.js
@@ -5,6 +5,15 @@ document.addEventListener("DOMContentLoaded", function() {
       const header = section.querySelector("h2");
       header.addEventListener("click", function() {
         section.classList.toggle("open");
+
+        
+      });const toggleButton = document.getElementById('toggle-button');
+      const allAdlTeamsSection = document.getElementById('all-adl-teams');
+      const preseasonAllAdlTeamsSection = document.getElementById('preseason-all-adl-teams');
+      
+      toggleButton.addEventListener('click', () => {
+        allAdlTeamsSection.style.display = allAdlTeamsSection.style.display === 'none'? 'block' : 'none';
+        preseasonAllAdlTeamsSection.style.display = preseasonAllAdlTeamsSection.style.display === 'none'? 'block' : 'none';
       });
     });
   });
